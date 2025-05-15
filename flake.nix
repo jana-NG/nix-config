955ad2978636502189332786fb071fc8e52cf21e
jana-NG <Jana.Goehlich@pm.me>
2025-05-15 12:34:10 +0200

    added cosmic nix due to it being endorsed on the cosmic site, untested

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
+    nixos-cosmic = {
+      url = "github:lilyinstarlight/nixos-cosmic";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nur = {
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -16,6 +20,7 @@
       nixpkgs,
       nix-flatpak,
       nur,
+      nixos-cosmic,
       ...
     }@inputs:
     let
@@ -26,10 +31,17 @@
       nixosConfigurations.nikkia485 = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         modules = [
+          {
+            nix.settings = {
+              substituters = [ "https://cosmic.cachix.org/" ];
+              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+            };
+          }
           nur.modules.nixos.default
           nix-flatpak.nixosModules.nix-flatpak
           ./configuration.nix
           ./flatpak.nix
+          nixos-cosmic.nixosModules.default
         ];
       };
     };
