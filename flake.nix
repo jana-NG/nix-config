58c37b73e574ab9db71feb080d760a257f452989
jana-NG <Jana.Goehlich@pm.me>
2025-05-19 11:33:55 +0200

    added cachix

--- flake.nix
+++ flake.nix
@@ -32,9 +32,13 @@
             nix.settings = {
               substituters = [
                 "https://cosmic.cachix.org/"
+                "https://nix-community.cachix.org"
                 "https://cache.nixos.org/"
               ];
-              trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
+              trusted-public-keys = [
+                "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
+                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+              ];
             };
           }
           nur.modules.nixos.default
