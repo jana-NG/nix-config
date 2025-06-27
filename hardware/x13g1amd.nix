7d7ddf57dc27df03668801dd5bbf22bbabbd48a7
jana-NG <Jana.Goehlich@pm.me>
2025-06-27 07:30:49 +0200

    clean up, new folder for hardware-configuration files, added tlp to x13g1amd.nix

--- hardware/x13g1amd.nix
+++ hardware/x13g1amd.nix
@@ -2,7 +2,7 @@
 { pkgs, ... }:
 {
   imports = [
-    ../hardware-configuration.nix
+    x13g1amd/hardware-configuration.nix
     ../packages/default.nix
     ../packages/dev.nix
     ../packages/home.nix
@@ -15,12 +15,32 @@
 
   boot.kernelParams = [
     "acpi_backlight=native"
+    "mem_sleep_default=deep"
   ];
   networking.hostName = "nikkix13g1"; # Define your hostname.
 
   # Power Management
   powerManagement.enable = true;
-  services.power-profiles-daemon.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+
+      #Optional helps save long term battery health
+      START_CHARGE_THRESH_BAT0 = 80; # 40 and below it starts to charge
+      STOP_CHARGE_THRESH_BAT0 = 90; # 80 and above it stops charging
+
+    };
+  };
 
   # Firmware updates
   hardware.enableRedistributableFirmware = true;
