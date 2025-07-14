5a9a09689e2938d13051dba8df8f1dc05d57c237
jana-NG <Jana.Goehlich@pm.me>
2025-07-14 20:59:00 +0200

    forgot to delete the moved cache servers, oops

--- flake.nix
+++ flake.nix
@@ -62,20 +62,6 @@
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
         modules = [
-          {
-            nix.settings = {
-              trusted-users = [ "nikki" ];
-              substituters = [
-                "https://cosmic.cachix.org/"
-                "https://nix-community.cachix.org"
-                "https://cache.nixos.org/"
-              ];
-              trusted-public-keys = [
-                "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-              ];
-            };
-          }
           nur.modules.nixos.default
           nix-flatpak.nixosModules.nix-flatpak
           ./hardware/a485.nix
