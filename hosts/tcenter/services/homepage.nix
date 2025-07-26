6f448be13e9b0049774e516dcfe719d716297f17
jana-NG <Jana.Goehlich@pm.me>
2025-07-26 17:52:40 +0200

    homepage improvements

--- hosts/tcenter/services/homepage.nix
+++ hosts/tcenter/services/homepage.nix
@@ -3,6 +3,8 @@
   services.homepage-dashboard = {
     enable = true;
     openFirewall = true;
+    listenPort = 80;
     package = pkgs-stable.homepage-dashboard;
+    allowedHosts = "tcenter.fritz.box:80";
   };
 }
