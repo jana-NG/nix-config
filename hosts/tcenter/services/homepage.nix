c191f4e07619d4f9bf33e719ecdc5401d07c1db6
jana-NG <Jana.Goehlich@pm.me>
2025-07-26 18:25:21 +0200

    testing homepage

--- hosts/tcenter/services/homepage.nix
+++ hosts/tcenter/services/homepage.nix
@@ -6,5 +6,31 @@
     listenPort = 8080;
     package = pkgs-stable.homepage-dashboard;
     allowedHosts = "tcenter.fritz.box:8080";
+    # https://gethomepage.dev/configs/settings/
+    settings = { };
+
+    # https://gethomepage.dev/configs/bookmarks/
+    bookmarks = [ ];
+
+    # https://gethomepage.dev/configs/services/
+    services = [ ];
+
+    # https://gethomepage.dev/widgets/
+    widgets = [
+      {
+        jellyfin = {
+          url = "http://tcenter.fritz.box:8096";
+          enableBlocks = true;
+          enableNowPlaying = true;
+          enableUser = true;
+        };
+      }
+      {
+        search = {
+          provider = "duckduckgo";
+          target = "_blank";
+        };
+      }
+    ];
   };
 }
