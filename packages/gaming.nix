c7c2c0f158de10493c3727b4aafb9650819282cb
jana-NG <Jana.Goehlich@pm.me>
2025-09-15 10:25:02 +0200

    added packages so steam could launch gamescope

--- packages/gaming.nix
+++ packages/gaming.nix
@@ -7,12 +7,17 @@
     package = pkgs.steam.override {
       extraPkgs =
         pkgs: with pkgs; [
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXinerama
+          xorg.libXScrnSaver
           libpng
           libpulseaudio
           libvorbis
           stdenv.cc.cc.lib # Provides libstdc++.so.6
           libkrb5
           keyutils
+          # Add other libraries as needed
         ];
     };
     remotePlay.openFirewall = true;
